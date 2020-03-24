9d310a259b94eac40cd077aa4bb7205b20d1cf76
Frank Boerman <frank@fboerman.nl>
2020-03-24 22:25:55 +0100

    make sure the brazil table time column is correct in the db

--- brazil/import.py
+++ brazil/import.py
@@ -4,6 +4,6 @@ from db import engine
 import pandas as pd
 
 df = pd.read_csv('saude.gov.br/brazil-timeseries-confirmed+deaths-perstate.csv', delimiter=';')
-
+df['time'] = pd.to_datetime(df['time'], format="%Y-%m-%d")
 print("[>] write to database")
 df.to_sql('brazil_states', engine, if_exists='replace')
