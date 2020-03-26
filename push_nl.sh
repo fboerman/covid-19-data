7e28d665eba3a6bf0dbe827dede8e06d321d2e44
Frank Boerman <frank@fboerman.nl>
2020-03-27 00:28:49 +0100

    fixed names in continent detection

--- push_nl.sh
+++ push_nl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-git add 'nederland/RIVM/*.csv'
+git add 'nederland/RIVM_timeseries/*.csv'
+git add 'nederland/RIVM_reports/*.pdf'
 git add 'world/COVID-19'
 git commit -m "auto data update" --author="Frank Boerman <frank@fboerman.nl>"
 git push
