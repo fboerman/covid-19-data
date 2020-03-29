eaf9ce9a2fb8ef4c4a9eaf060d6c68034560ab93
Frank Boerman <frank@fboerman.nl>
2020-03-29 17:53:57 +0200

    fixed import script rivm reports due to new format, added skiplist for import world

--- nederland/import_reports.py
+++ nederland/import_reports.py
@@ -10,8 +10,9 @@ reports = [x for x in list(os.walk("RIVM_reports"))[0][2] if x.split('.')[-1] ==
 reports.sort()
 
 dfs = tabula.read_pdf("RIVM_reports/"+reports[-1], pages='all', multiple_tables=True)
-df_age = dfs[0]
-df_sex = dfs[1]
+df_sex = None
+df_age = None
+df_hospital = None
 
 for df in dfs:
     try:
@@ -26,38 +27,52 @@ for df in dfs:
         df_age = df
         continue
     try:
-        if df.columns[2] == 'Ziekenhuisopname' and len(df.columns) == 3:
+        if 'Ziekenhuis' in df.columns[2] and len(df.columns) == 3:
             df_hospital = df
+            df_hospital.columns = ['Ziekenhuisopname' if 'Ziekenhuis' in x else x for x in df_hospital.columns]
             continue
     except:
         pass
 
+if df_age is not None:
+    df_age.drop([0], inplace=True)
+    #df_age.drop([x for x in df_age.columns if '%' in x], axis=1, inplace=True)
+    df_age.columns = list(range(len(df_age.columns)))
+    df_age.drop([2,4], axis=1, inplace=True)
+    df_age.columns = ['Leeftijdsgroep', 'Totaal', 'Ziekenhuisopname', 'Overleden']
+    df_age['Overleden'] = df_age['Overleden'].apply(lambda x: x.split(' ')[0])
+    df_age.set_index("Leeftijdsgroep", inplace=True)
+    df_age = df_age.astype('int')
 
-df_age.drop([0,21], inplace=True)
-df_age.drop(["%", "%.1", "%.2"], axis=1, inplace=True)
-df_age.set_index("Leeftijdsgroep", inplace=True)
-df_age = df_age.astype('int')
+    df_age["Totaal_per"] = round(df_age["Totaal"]/df_age["Totaal"].sum() * 100,2)
+    df_age["Ziekenhuis_per"] = round(df_age["Ziekenhuisopname"]/df_age["Ziekenhuisopname"].sum() * 100,2)
+    df_age["Overleden_per"] = round(df_age["Overleden"]/df_age["Overleden"].sum() * 100,2)
+else:
+    print("[!] could not find df_age")
 
-df_age["Totaal_per"] = round(df_age["Totaal"]/df_age["Totaal"].sum() * 100,2)
-df_age["Ziekenhuis_per"] = round(df_age["Ziekenhuisopname"]/df_age["Ziekenhuisopname"].sum() * 100,2)
-df_age["Overleden_per"] = round(df_age["Overleden"]/df_age["Overleden"].sum() * 100,2)
+if df_sex is not None:
+    df_sex.drop(["%", "%.1", "%.2"], inplace=True, axis=1)
+    df_sex.drop([0], inplace=True)
+    df_sex.columns = ['Geslacht', 'Totaal', 'Ziekenhuisopname', 'Overleden']
+    df_sex.set_index('Geslacht', inplace=True)
+else:
+    print("[!] could not find df_sex")
 
-df_sex.drop(["%", "%.1", "%.2"], inplace=True, axis=1)
-df_sex.drop([0], inplace=True)
-df_sex.columns = ['Geslacht', 'Totaal', 'Ziekenhuisopname', 'Overleden']
-df_sex.set_index('Geslacht', inplace=True)
+if df_hospital is not None:
+    df_hospital.drop([0], inplace=True)
+    df_hospital['Totaal_add'] = df_hospital['Totaal'].cumsum()
+    df_hospital['Ziekenhuisopname_add'] = df_hospital['Ziekenhuisopname'].cumsum()
+    df_hospital['Ziekenhuisopname_percentage'] = round(df_hospital['Ziekenhuisopname_add']/df_hospital['Totaal_add']*100,2)
+    c = list(df_hospital.columns)
+    c[0] = 'time'
+    df_hospital.columns = c
+    df_hospital['time'] = pd.to_datetime(df_hospital['time'], format="%Y-%m-%d")
+else:
+    print("[!] could not find df_hospital")
 
-df_hospital.drop([0], inplace=True)
-df_hospital['Totaal_add'] = df_hospital['Totaal'].cumsum()
-df_hospital['Ziekenhuisopname_add'] = df_hospital['Ziekenhuisopname'].cumsum()
-df_hospital['Ziekenhuisopname_percentage'] = round(df_hospital['Ziekenhuisopname_add']/df_hospital['Totaal_add']*100,2)
-c = list(df_hospital.columns)
-c[0] = 'time'
-df_hospital.columns = c
-df_hospital['time'] = pd.to_datetime(df_hospital['time'], format="%Y-%m-%d")
-
-
-
-df_age.to_sql('netherlands_rivm_current_age', engine, if_exists='replace')
-df_sex.to_sql('netherlands_rivm_current_sex', engine, if_exists='replace')
-df_hospital.to_sql('netherlands_rivm_hospitals', engine, if_exists='replace')
+if df_age is not None:
+    df_age.to_sql('netherlands_rivm_current_age', engine, if_exists='replace')
+if df_sex is not None:
+    df_sex.to_sql('netherlands_rivm_current_sex', engine, if_exists='replace')
+if df_hospital is not None:
+    df_hospital.to_sql('netherlands_rivm_hospitals', engine, if_exists='replace')
--- world/import.py
+++ world/import.py
@@ -72,7 +72,7 @@ df=pd.concat([df_confirmed,df_deaths])#,df_recovered])
 df_diff=pd.concat([df_confirmed_diff,df_deaths_diff])#,df_recovered_diff])
 
 # function to retrieve continent with some handling of special cases since WHO does not fully follow ISO
-get_continent = lambda cname: 'EU' if  cname in ['Reunion','Channel Islands', 'Holy See', 'Saint Barthelemy', 'Kosovo'] else 'AS' if cname in ['Hong Kong SAR', 'Macao SAR', 'occupied Palestinian territory', 'West Bank and Gaza'] else 'AF' if cname in ['Cote d\'Ivoire', 'Congo (Brazzaville)'] else 'OC' if cname in ['Timor-Leste'] else  pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(cname))
+get_continent = lambda cname: 'EU' if  cname in ['Reunion','Channel Islands', 'Holy See', 'Saint Barthelemy', 'Kosovo'] else 'AS' if cname in ['Burma', 'Hong Kong SAR', 'Macao SAR', 'occupied Palestinian territory', 'West Bank and Gaza'] else 'AF' if cname in ['Cote d\'Ivoire', 'Congo (Brazzaville)'] else 'OC' if cname in ['Timor-Leste'] else  pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(cname))
 
 # create the time column needed for grafana
 df.reset_index(level=0, inplace=True)
@@ -89,7 +89,13 @@ countries = list(df_confirmed.columns)
 countries.remove('type')
 countries.remove('Diamond Princess')
 for continent in ['AF', 'AS', 'EU', 'NA', 'OC', 'SA']:
-    countries_select = [c for c in countries if get_continent(c) == continent]
+    countries_select = []
+    for c in countries:
+        if c in ['MS Zaandam']:
+            continue
+        if get_continent(c) == continent:
+            countries_select.append(c)
+#    countries_select = [c for c in countries if get_continent(c) == continent]
     df[['time'] + list(countries_select) + ['type']].to_sql('world_timeseries_'+continent, engine, if_exists='replace')
     df_diff[['time'] + list(countries_select) + ['type']].to_sql('world_timeseries_'+continent+'_diff', engine, if_exists='replace')
 
