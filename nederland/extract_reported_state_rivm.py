c2120868e7bea8c91b940820233937a6363811c5
Frank Boerman <frank@fboerman.nl>
2020-04-03 14:15:55 +0200

    added extract current state from rivm site

---
+++ nederland/extract_reported_state_rivm.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import re
+import pandas as pd
+from db import engine
+
+try:
+    r = requests.get("https://www.rivm.nl/coronavirus-kaart-van-nederland", timeout=10)
+except:
+    print("[!] Could not retrieve rivm site: unreachable")
+    sys.exit(-1)
+if r.status_code != 200:
+    print("[!] Could not retrieve rivm site: {}".format(r.status_code))
+    sys.exit(-1)
+
+soup = BeautifulSoup(r.text, 'lxml')
+# cards = soup.find('div', {'class':['card-wrapper', 'top']})
+# numbers = [int(re.sub(r'[^0-9]', '', x.text)) for x in cards.find_all('span', class_='h3')]
+# numbers_diff = [int(x) for x in [re.sub(r'[^0-9]', '', x.text) for x in cards.find_all('p') if '+' in str(x)] if x != '']
+rows = soup.find('table').find_all('tr')
+numbers = [int(re.sub(r'[^0-9]', '', x.text)) for x in rows[1].find_all('td')]
+numbers_diff = [int(re.sub(r'[^0-9]', '', x.text)) for x in rows[2].find_all('td')]
+
+df = pd.DataFrame([
+    numbers,
+    numbers_diff
+], columns=['confirmed', 'admissions', 'deaths'])
+
+df.to_sql('netherlands_rivm_current', engine, if_exists='replace')
