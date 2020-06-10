b5a200edf1539dcb80accb06acdfd52982c62ec3
Frank Boerman <frank@fboerman.nl>
2020-06-10 17:07:42 +0200

    also push per pop csv

--- push_nl.sh
+++ push_nl.sh
@@ -3,5 +3,6 @@
 git add 'nederland/*.csv'
 git add 'nederland/RIVM_reports/*.pdf'
 git add 'world/COVID-19'
+git add 'world/world_timeseries_per_pop.csv'
 git commit -m "auto data update NL and world data" --author="Frank Boerman <frank@fboerman.nl>"
 git push
