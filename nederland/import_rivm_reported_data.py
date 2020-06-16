4b43bc3957356a98e6b7b4c2f7e987bb86f5e5cb
Frank Boerman <frank@fboerman.nl>
2020-06-16 18:44:23 +0200

    fixed changed naming of rivm graphs

--- nederland/import_rivm_reported_data.py
+++ nederland/import_rivm_reported_data.py
@@ -3,7 +3,10 @@
 import pandas as pd
 import json
 from datetime import date
-from db import engine
+try:
+    from db import engine
+except:
+    engine = None
 
 with open('rivm_graphs.json', 'r') as stream:
     data = json.load(stream)
@@ -16,6 +19,7 @@ df_deaths = None
 maanden = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec']
 maand_to_num = lambda x, maanden=maanden: maanden.index(x) + 1
 
+
 def to_df(data):
     df = pd.DataFrame(data[1:], columns=['time', 'nieuw', 'gemeld'])
     df = df.astype({'nieuw': 'int', 'gemeld': 'int'})
@@ -24,25 +28,26 @@ def to_df(data):
                        day=int(x.replace('-', ' ').split(' ')[0])))
     return df
 
+
 for key, easychart in data['easychart'].items():
     config = json.loads(easychart['config'])
-    data = json.loads(easychart['csv'])
+    data_csv = json.loads(easychart['csv'])
     if config['title']['text'] == 'Leeftijd en geslacht overledenen':
-        df_age = pd.DataFrame(data[1:], columns=['Leeftijdsgroep', 'Man', "Vrouw"])
+        df_age = pd.DataFrame(data_csv[1:], columns=['Leeftijdsgroep', 'Man', "Vrouw"])
         df_age = df_age.astype({'Man': 'int', 'Vrouw': 'int'})
         df_age['Overleden'] = df_age['Man'] + df_age['Vrouw']
-    elif config['title']['text'] == 'Overledenen per dag':
-        df_deaths = to_df(data)
+    elif config['title']['text'] == 'Overledenen per dag vanaf 27 februari 2020':
+        df_deaths = to_df(data_csv)
         df_deaths['deaths'] = df_deaths['nieuw'] + df_deaths['gemeld']
         df_deaths['deaths_cum'] = df_deaths['deaths'].cumsum()
         df_deaths.drop(['nieuw', 'gemeld'], axis=1, inplace=True)
-    elif config['title']['text'] == 'In ziekenhuis opgenomen patiënten':
-        df_hospital = to_df(data)
+    elif config['title']['text'] == 'In ziekenhuis opgenomen patiënten vanaf 27 februari 2020':
+        df_hospital = to_df(data_csv)
         df_hospital['Ziekenhuisopname'] = df_hospital['nieuw'] + df_hospital['gemeld']
         df_hospital['Ziekenhuisopname_add'] = df_hospital['Ziekenhuisopname'].cumsum()
         df_hospital.drop(['nieuw', 'gemeld'], axis=1, inplace=True)
-    elif config['title']['text'] == 'Bij de GGD gemelde patiënten':
-        df_confirmedcases = to_df(data)
+    elif config['title']['text'] == 'Bij de GGD gemelde patiënten vanaf 27 februari 2020':
+        df_confirmedcases = to_df(data_csv)
         df_confirmedcases['Cases'] = df_confirmedcases['nieuw'] + df_confirmedcases['gemeld']
         df_confirmedcases['Cases_add'] = df_confirmedcases['Cases'].cumsum()
         df_confirmedcases.drop(['nieuw', 'gemeld'], axis=1, inplace=True)
@@ -54,13 +59,16 @@ df_hospital.rename({'time_x': 'time'}, axis=1, inplace=True)
 
 if df_age is not None:
     name = 'netherlands_rivm_current_age'
-    df_age.to_sql(name, engine, if_exists='replace')
+    if engine is not None:
+        df_age.to_sql(name, engine, if_exists='replace')
     df_age.to_csv('RIVM_reports/csv_latest/{}.csv'.format(name), sep=';', index=False)
 if df_deaths is not None:
     name = 'netherlands_rivm_deaths'
-    df_deaths.to_sql(name, engine, if_exists='replace')
+    if engine is not None:
+        df_deaths.to_sql(name, engine, if_exists='replace')
     df_deaths.to_csv('RIVM_reports/csv_latest/{}.csv'.format(name), sep=';', index=False)
 if df_hospital is not None:
     name = 'netherlands_rivm_hospitals'
-    df_hospital.to_sql(name, engine, if_exists='replace')
+    if engine is not None:
+        df_hospital.to_sql(name, engine, if_exists='replace')
     df_hospital.to_csv('RIVM_reports/csv_latest/{}.csv'.format(name), sep=';', index=False)
